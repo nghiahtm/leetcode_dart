71ab20f06cf6018242746ca878d7f7402820ea8a
HTMN <nghia.htm97@gmail.com>
2025-12-25 16:37:29 +0700

    update leet11

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,9 +1,11 @@
-import 'package:dart_application_1/leetcode_70.dart';
+import 'package:dart_application_1/leetcode_11.dart';
 
 void main(List<String> arguments) {
   final sol = Solution();
-  int res1 = sol.climbStairs(3);
+  DateTime startTime = DateTime.now();
+  int res1 = sol.maxArea([10, 14, 10, 4, 10, 2, 6, 1, 6, 12]);
+  DateTime endTime = DateTime.now();
+  Duration duration = endTime.difference(startTime);
   print("res1: $res1");
-  int res2 = sol.climbStairs(6);
-  print("res2: $res2");
+  print('Thời gian thực thi thuật toán: ${duration.inMilliseconds}ms');
 }
